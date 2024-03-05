5215a508bc59d5b1e551f1552f18f3c0497e260d
plucik <deliverycrunchtime@gmail.com>
2024-03-05 13:37:46 +0200

    improves scrolling ui

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/components/DatabaseTabs.vue
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/components/DatabaseTabs.vue
@@ -186,7 +186,13 @@ export default {
             this.$nextTick(() => {
               let lastTab = document.getElementById(this.selectedTab.id);
               if (!!lastTab) {
-                lastTab.scrollIntoView();
+                setTimeout(() => {
+                  lastTab.scrollIntoView({
+                    block: "end",
+                    inline: "end",
+                    behavior: "smooth",
+                  });
+                }, 100);
               }
             });
           }
@@ -700,16 +706,28 @@ export default {
     },
     handleNavWheel(event) {
       event.deltaY > 0
-        ? (this.$refs.navTabs.scrollLeft += 100)
-        : (this.$refs.navTabs.scrollLeft -= 100);
+        ? this.$refs.navTabs.scrollTo({
+            left: this.$refs.navTabs.scrollLeft + 100,
+            behavior: "smooth",
+          })
+        : this.$refs.navTabs.scrollTo({
+            left: this.$refs.navTabs.scrollLeft - 100,
+            behavior: "smooth",
+          });
       this.updateScrollOptions();
     },
     handleLeftScrollClick(event) {
-      this.$refs.navTabs.scrollLeft -= 100;
+      this.$refs.navTabs.scrollTo({
+        left: this.$refs.navTabs.scrollLeft - 100,
+        behavior: "smooth",
+      });
       this.updateScrollOptions();
     },
     handleRightScrollClick(event) {
-      this.$refs.navTabs.scrollLeft += 100;
+      this.$refs.navTabs.scrollTo({
+        left: this.$refs.navTabs.scrollLeft + 100,
+        behavior: "smooth",
+      });
       this.updateScrollOptions();
     },
     updateScrollOptions() {
