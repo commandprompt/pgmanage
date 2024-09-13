082a57e922926949ea241ca1390a952fdb341de4
plucik <deliverycrunchtime@gmail.com>
2024-09-13 14:36:08 +0300

    iterate over tab_ids instead of tab objects, adds saved tab_db_id to tab metadata after query request refs: #475

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/stores/tabs.js
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/stores/tabs.js
@@ -345,9 +345,9 @@ const useTabsStore = defineStore("tabs", {
               this.beforeCloseTab(e, () => {
                 let tabsToRemove = [];
 
-                let tabs = this.getSecondaryTabs(primaryTab.id);
-
-                tabs.forEach((tab) => {
+                let tabs_id = this.getSecondaryTabs(primaryTab.id).map(tab => tab.id);
+                tabs_id.forEach((tab_id) => {
+                  let tab = this.getSecondaryTabById(tab_id, primaryTab.id);
                   if (
                     tab.metaData.mode == "query" ||
                     tab.metaData.mode == "edit" ||
@@ -362,6 +362,8 @@ const useTabsStore = defineStore("tabs", {
                       tab_db_id: null,
                       conn_tab_id: primaryTab.id,
                     };
+                    console.log(tab)
+                    console.log(tab.metaData.initTabDatabaseId, 'tab_db_id')
                     if (tab.metaData.mode == "query")
                       messageData.tab_db_id = tab.metaData.initTabDatabaseId;
                     tabsToRemove.push(messageData);
