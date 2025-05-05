179e58ef2764b03ab45e8c8c3bd5f8186f921301
plucik <deliverycrunchtime@gmail.com>
2025-05-05 15:03:03 +0300

    trigger ddl on power tree node toggle, minor fixes

--- pgmanage/app/static/pgmanage_frontend/src/mixins/power_tree.js
+++ pgmanage/app/static/pgmanage_frontend/src/mixins/power_tree.js
@@ -72,6 +72,9 @@ export default {
     },
     onToggle(node, e) {
       this.$refs.tree.select(node.path);
+      if (this.getRootNode().title !== "Snippets") {
+        this.getProperties(node);
+      }
       if (node.isExpanded) return;
       this.refreshTree(node);
       if(settingsStore.scrollTree) {
