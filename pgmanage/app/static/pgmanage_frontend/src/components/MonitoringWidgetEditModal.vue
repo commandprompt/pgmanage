ce1f3ed1baf72fff22ae178a82ca28d0a410f857
Eugene Dubinin <eugend@commandprompt.com>
2025-05-16 14:06:29 +0300

    removed unnecessary validation feedback placeholders

--- pgmanage/app/static/pgmanage_frontend/src/components/MonitoringWidgetEditModal.vue
+++ pgmanage/app/static/pgmanage_frontend/src/components/MonitoringWidgetEditModal.vue
@@ -103,7 +103,6 @@
                   data-testid="widget-edit-template-select"
                   :class="[
                     'form-select',
-                    { 'is-invalid': v$.selectedWidget.$invalid },
                   ]"
                   v-model="selectedWidget"
                   @change="changeTemplate"
@@ -118,12 +117,6 @@
                     ({{ widget.type }}) {{ widget.title }}
                   </option>
                 </select>
-                <div class="invalid-feedback">
-                  <a v-for="error of v$.selectedWidget.$errors" :key="error.$uid">
-                    {{ error.$message }}
-                    <br />
-                  </a>
-                </div>
               </div>
             </div>
 
