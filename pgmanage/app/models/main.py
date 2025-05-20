5a28257e44f2c088787c5a73a43d06529bd49d80
plucik <deliverycrunchtime@gmail.com>
2025-05-20 12:26:13 +0300

    adds pin/unpin database functionality

--- pgmanage/app/models/main.py
+++ pgmanage/app/models/main.py
@@ -82,6 +82,7 @@ class Connection(models.Model):
     last_access_date = models.DateTimeField(null=True)
     autocomplete = models.BooleanField(default=True)
     color_label = models.IntegerField(default=0)
+    pinned_databases = models.JSONField(default=list)
 
     @classmethod
     def reencrypt_credentials(cls, user_id: int, old_key: str, new_key: str) -> None:
--- pgmanage/app/views/tree_postgresql.py
+++ pgmanage/app/views/tree_postgresql.py
@@ -1,8 +1,10 @@
 import ast
 
+from app.models.main import Connection
 from app.utils.crypto import pg_scram_sha256
 from app.utils.decorators import database_required, user_authenticated
 from django.http import HttpResponse, JsonResponse
+
 
 @user_authenticated
 @database_required(check_timeout=True, open_connection=True, prefer_database='postgres')
@@ -821,6 +823,7 @@ def get_schemas(request, database):
 def get_databases(request, database):
     list_databases = []
 
+    conn_object = Connection.objects.get(id=database.v_conn_id)
     try:
         databases = database.QueryDatabases()
         for database_object in databases.Rows:
@@ -828,6 +831,8 @@ def get_databases(request, database):
                 "name": database_object["database_name"],
                 "name_raw": database_object["name_raw"],
                 "oid": database_object["oid"],
+                "pinned": database_object["database_name"]
+                in conn_object.pinned_databases,
             }
             list_databases.append(database_data)
     except Exception as exc:
